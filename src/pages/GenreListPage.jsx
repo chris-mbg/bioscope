96e3a4b29852b162ded450c09582217bf2bf3b74
Christina <chrasse@hotmail.com>
2021-09-14 15:40:24 +0200

    Fixed heading on GenrePage by sending state with the Link on GenreListPage

--- src/pages/GenreListPage.jsx
+++ src/pages/GenreListPage.jsx
@@ -12,7 +12,8 @@ const GenreListPage = () => {
     ["all-genres"],
     getAllGenres,
     {
-      cacheTime: 1000 * 60 * 30,
+      staleTime: 1000 * 60 * 60,        //1h
+      cacheTime: 1000 * 60 * 60 * 24, // 24h
     }
   );
 
@@ -21,11 +22,25 @@ const GenreListPage = () => {
       <h1>Genres</h1>
       <LoadError isLoading={isLoading} isError={isError} error={error} />
       {data && (
-        <Row xs={2} sm={3} md={4} lg={5} className="mx-auto" id="genre-names-links">
+        <Row
+          xs={2}
+          sm={3}
+          md={4}
+          lg={5}
+          className="mx-auto"
+          id="genre-names-links"
+        >
           {data.genres.map((genre) => (
             <Col key={genre.id}>
-              <Link to={`/genres/genre/${genre.id}`} >
-                <div className="background-opacity-dark fs-5 text-center py-1 py-md-3 my-1">{genre.name}</div>
+              <Link
+                to={{
+                  pathname: `/genres/genre/${genre.id}`,
+                  state: { genreName: genre.name },
+                }}
+              >
+                <div className="background-opacity-dark fs-5 text-center py-1 py-md-3 my-1">
+                  {genre.name}
+                </div>
               </Link>
             </Col>
           ))}
--- src/pages/GenrePage.jsx
+++ src/pages/GenrePage.jsx
@@ -2,28 +2,21 @@ import React, { useState, useEffect } from "react";
 import { useQuery, useQueryClient } from "react-query";
 import { getMoviesForGenre } from "../services/TMDBAPI";
 import { useParams } from "react-router";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Pagination from "../components/utilities/Pagination";
 import { useUrlSearchParams } from "use-url-search-params";
 import MoviesWrapper from "../components/movies/MoviesWrapper";
 import LoadError from "../components/utilities/LoadError";
 
-
 const GenrePage = () => {
   const { id: genreId } = useParams();
+
+  // From the GenreListPage the name of the genre is sent via the Link as state on the location object. The state is then used in the JSX to write out what genre the movies belongs to.
+  const location = useLocation();
   const queryClient = useQueryClient();
   const [page, setPage] = useUrlSearchParams({ page: 1 }, { page: Number });
 
-  // Trying to ge genre name from cached data... Do not work on hard reload, and destroys the ability to go back in the browser?!?
-  // const [genreName, setGenreName] = useState('');
-  // const genreArr = queryClient.getQueryData(["all-genres"]);
-
-  // useEffect(() => {
-  //   const item = genreArr?.genres.filter(gen => gen.id == genreId)
-  //   if(item) {
-  //     setGenreName(item[0].name)
-  //   }
-  // }, [])
+  console.log(location);
 
   const { data, isLoading, isError, error, isPreviosData } = useQuery(
     ["genre-movies", genreId, page],
@@ -33,7 +26,10 @@ const GenrePage = () => {
 
   return (
     <>
-      <h1 className="text-center">Find ... Movies</h1>
+      <h1 className="text-center">
+        {/* If a hard reload is made on the genre page, the genre name will no longer be specified due to that state on the location object is undefined */}
+        Find {location.state?.genreName ?? ""} Movies
+      </h1>
 
       <LoadError isLoading={isLoading} isError={isError} error={error} />
 
